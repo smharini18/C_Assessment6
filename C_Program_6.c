bb8ba887ee8d1210dbf8f0f6ad124629ed756208
smharini18 <smharini18@gmail.com>
2026-06-10 19:27:37 +0530

    Create C_Program_6.c

---
+++ C_Program_6.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main()
+{
+    int i=11;
+    while(i<=20)
+    {
+        printf("%d\n",i);
+        i=i+2;
+        
+    }
+  
+}
