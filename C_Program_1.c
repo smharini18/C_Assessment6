c709c4e2914a887ec4348da981443cc8ce7dd3c3
smharini18 <smharini18@gmail.com>
2026-06-10 19:09:21 +0530

    Create C_Program_1.c

---
+++ C_Program_1.c
@@ -0,0 +1,13 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int i=0;
+    while(i<5)
+    {
+        i=i+1;
+        printf("%d\n",i);
+    }
+
+    return 0;
+}
