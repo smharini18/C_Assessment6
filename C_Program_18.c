ed5be60abca1d8af502e62219d38c14546e83970
smharini18 <smharini18@gmail.com>
2026-06-12 20:17:26 +0530

    Create C_Program_18.c

---
+++ C_Program_18.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c, d, i,prime;
+    printf("Enter the Number : ");
+    scanf("%d",&a);
+    b=a%10;
+    c=(a/10)%10;
+    d=c*10+b;
+    if(d<2)
+    {
+    printf("Not prime");
+    return 0;
+    }
+    i=2;
+    prime=1;
+    while(i<d)
+    {
+    if(d%i==0)
+    {
+    prime=0;
+    }
+    i++;
+    }
+    if(prime==1) 
+    {
+    printf("Prime");
+    }
+    else
+    {
+    printf("Not prime");
+    }
+}
