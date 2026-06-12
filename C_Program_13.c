67d22a94c953188a28fd265b2a1c57f47f8ee01e
smharini18 <smharini18@gmail.com>
2026-06-12 20:08:33 +0530

    Create C_Program_13.c

---
+++ C_Program_13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter the Number : ");
+    scanf("%d",&a);
+    while(a!=0)
+    {
+    b=a%10;
+    a=a/10;
+    printf("%d",b);
+    }
+    
+}
