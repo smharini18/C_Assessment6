44105e1f9648c50796febcf04362a8bb6850b2c2
smharini18 <smharini18@gmail.com>
2026-06-12 20:13:34 +0530

    Create C_Program_15.c

---
+++ C_Program_15.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c=0,d, g, m, p=1,i=1,w;
+    printf("Enter the Number : ");
+    scanf("%d",&a);
+    m=a%10;
+    g=a;
+    while(g!=0)
+    {
+    g=g/10;
+    if(g!=0)
+    {
+    c=c+1;
+    }
+    }
+    i=1;
+    while(i<=c)
+    {
+    p=p*10;
+    i++;
+    }
+    w=a%p;
+    d=a/p;
+    if(m%2==1)
+    {
+    printf("%d%d",d-1,w);
+    }
+    else
+    {
+    printf("%d%d",d,w);
+    }
+    
+    
+}
