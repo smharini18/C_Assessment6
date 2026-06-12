9dedfb73c346884fd8797b8feba90d7471dd54fb
smharini18 <smharini18@gmail.com>
2026-06-12 20:11:29 +0530

    Create C_Program_14.c

---
+++ C_Program_14.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, c=0,d, g, m, p=1,i=1,w;
+    printf("Enter the Number : ");
+    scanf("%d",&a);
+    m=a/10;
+    b=a%10;
+    g=a;
+    while(g!=0)
+    {
+    g=g/10;
+    if(g!=0)
+    {
+    c=c+1;
+    }
+    }
+    i=1;
+    while(i<c)
+    {
+    p=p*10;
+    i++;
+    }
+    w=m%p;
+    d=a/(p*10);
+    printf("%d%d%d",b,w,d);
+    
+    
+}
