a7a4a428e30422ad29216e35d8af355d7974261b
smharini18 <smharini18@gmail.com>
2026-06-13 22:51:07 +0530

    Create C_Program_24.c

---
+++ C_Program_24.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+     int a, b, c=0,d,m, n;
+     printf("Enter the Number : ");
+     scanf("%d",&a);
+     while(a>10)
+     {
+     b=a%10;
+     m=(a/10)%10;
+     n=m*10+b;
+     d=sqrt(n);
+     if(d*d==n)
+     {
+     c=c+1;
+     }
+     a=a/10;
+     }
+     printf("%d",c);
+}
