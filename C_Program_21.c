8ab02b99fd4694d36c05067cbcf91e46765e9752
smharini18 <smharini18@gmail.com>
2026-06-13 22:46:23 +0530

    Create C_Program_21.c

---
+++ C_Program_21.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+     int a, b, c=0;
+     printf("Enter the Number : ");
+     scanf("%d",&a);
+     while(a!=0)
+     {
+     b=a%10;
+     if(b%2==1)
+     {
+     c=c+1;
+     }
+     a=a/10;
+     }
+     printf("%d",c);
+}
