a152915d088c2d2c086751cec66f82993e300144
smharini18 <smharini18@gmail.com>
2026-06-13 22:53:44 +0530

    Create C_Program_25.c

---
+++ C_Program_25.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main()
+{
+     int a, b, c=0,i,prime;
+     printf("Enter the Number : ");
+     scanf("%d",&a);
+     while(a!=0)
+     {
+     b=a%10;
+     if(b<2)
+     {
+     prime=0;
+     }
+     else
+     {
+     i=2;
+     prime=1;
+     while(i<b)
+     {
+     if(b%i==0)
+     {
+     prime=0;
+     }
+     i++;
+     }
+     }
+     if(prime==1) 
+     {
+     c=c+1;
+     }
+     a=a/10;
+     }
+     printf("%d",c);
+     
+}
